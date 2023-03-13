9ea34a3599b4ffa7e2aa9a8a4f9be7ae0501b03a
ys <young020920@gmail.com>
2023-03-13 17:47:56 +0900

    초기 커밋.

---
+++ LeastSquare/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace LeastSquare
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picdraw = new System.Windows.Forms.PictureBox();
+            this.btn1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picdraw)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picdraw
+            // 
+            this.picdraw.BackColor = System.Drawing.Color.White;
+            this.picdraw.Location = new System.Drawing.Point(35, 12);
+            this.picdraw.Name = "picdraw";
+            this.picdraw.Size = new System.Drawing.Size(500, 500);
+            this.picdraw.TabIndex = 0;
+            this.picdraw.TabStop = false;
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(597, 38);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(153, 89);
+            this.btn1.TabIndex = 1;
+            this.btn1.Text = "button1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 562);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.picdraw);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picdraw)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picdraw;
+        private System.Windows.Forms.Button btn1;
+    }
+}
+
---
+++ LeastSquare/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LeastSquare
+{
+    public partial class Form1 : Form
+    {
+        double xmin = -2, xmax = 7;
+        double ymin = -5, ymax = 70;
+        private float xpixel(double xw)
+        {
+            return (float)(picdraw.ClientSize.Width * (xw - xmin) / (xmax - xmin));
+        }
+        private float ypixel(double yw)
+        {
+            return (float)(picdraw.ClientSize.Height * (1-(yw - ymin) / (ymax - ymin)));
+        }
+        private float matrix1(double m1,double m2, double m3)
+        {
+            double[] a = new double[3] { m1, m2, m3 };
+            return 0;
+        }
+        private double d(double a11,double a12, double a13, double a21,double a22, double a23, double a31, double a32,double a33)
+        {
+            double sum;
+            sum = a11 * (a22 * a33 - a23 * a32) - a12 * (a21 * a33 - a23 * a31) + a13 * (a21 * a32 - a22 * a31);
+            return sum;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //데이터세팅
+            const int n=6;
+            double[] xw = new double[n] { 1, 2, 3, 4, 5, 6 };
+            double[] yw = new double[n] { 2.1, 7.7, 13.6, 27.2, 40.9, 61.1 };
+            //좌표그리기
+            Graphics grp = picdraw.CreateGraphics();
+            grp.DrawLine(new Pen(Color.Black), xpixel(xmin), ypixel(0),
+                xpixel(xmax), ypixel(0));
+            grp.DrawLine(new Pen(Color.Black), xpixel(0), ypixel(ymin),
+               xpixel(0), ypixel(ymax));
+           
+            for (int i = 0; i < n; i++)
+            {
+                grp.DrawEllipse(new Pen(Color.Red), xpixel(xw[i]), ypixel(yw[i]),2,2);
+            }
+            double sumXY = 0, sumX = 0, sumY = 0, sumXX = 0, sumXXX = 0, sumXXXX=0,sumXXY=0 ;
+            for(int i=0;i<n;i++)
+            {
+                sumXY += xw[i] * yw[i];
+                sumX += xw[i];
+                sumY += yw[i];
+                sumXX += xw[i] * xw[i];
+                sumXXX += xw[i] * xw[i]*xw[i];
+                sumXXXX += xw[i] * xw[i] * xw[i]*xw[i];
+                sumXXY += xw[i] * xw[i]*yw[i];
+            }
+            double d4 = d(n, sumX, sumXX, sumX, sumXX, sumXXX, sumXX, sumXXX, sumXXXX);
+            double d0 = d(sumY, sumX, sumXX, sumXY, sumXX, sumXXX, sumXXY, sumXXX, sumXXXX);
+            double d1 = d(n, sumY, sumXX, sumX, sumXY, sumXXX, sumXX, sumXXY, sumXXXX);
+            double d2 = d(n, sumX, sumY, sumX, sumXX, sumXY, sumXX,sumXXX, sumXXY);
+
+            double a0 = d0 / d4;
+            double a1 = d1 / d4;
+            double a2 = d2 / d4;
+
+            double prexw, preyw,x=0,y=0;
+
+            for (prexw = xmin; prexw < xmax; prexw += 0.01)
+            {
+                preyw = a0 + a1 * prexw + a2 * prexw * prexw;
+
+                if (prexw == xmin) {
+                    grp.DrawLine(new Pen(Color.Red), xpixel(prexw), ypixel(preyw), xpixel(prexw), ypixel(preyw));
+                    x = prexw;
+                    y = preyw;
+                }
+                    
+                grp.DrawLine(new Pen(Color.Red), xpixel(x), ypixel(y), xpixel(prexw), ypixel(preyw));
+                x = prexw;
+                y = preyw;
+
+            }
+             
+            
+        }
+    }
+}
